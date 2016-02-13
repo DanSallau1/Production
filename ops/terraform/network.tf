7cf3e010f0708fd810fe728fe8f6ea89a35d332e
CruzanCaramele <letters2hamza@gmail.com>
2016-02-14 01:47:36 +0800

    combine networking,sg groups and dns

--- ops/terraform/network.tf
+++ ops/terraform/network.tf
@@ -95,6 +95,51 @@ resource "aws_route_table_association" "public" {
 		create_before_destroy =true
 	}
 }
+#--------------------------------------------------------------
+# elb
+#--------------------------------------------------------------
+resource "aws_elb" "elb" {
+	name = "elb"
+	subnets = ["${aws_subnet.public.id}", "${aws_subnet.private.id}"]
+	security_groups = ["${aws_security_group.web.id}", "${aws_security_group.consul-security.id}"]
+
+	listener {
+		instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+	}
+
+	health_check {
+		healthy_threshold = 2
+		unhealthy_threshold = 2
+		timeout = 3
+		target = "HTTP:80/"
+		interval = 30
+	}
+
+	instances = ["${aws_instance.culturely-web.*.id}","${aws_instance.consul-web.*.id}"]
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+#--------------------------------------------------------------
+# DNS(Route53)
+#--------------------------------------------------------------
+resource "aws_route53_record" "dns" {
+	zone_id = "{aws_route53_zone.primary.zone_id}"
+	name    = "linuxacademylab23.com"
+	type = "CNAME"
+	ttl = "300"
+	records = ["${aws_elb.elb.dns_name}"]
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
 
 
 
