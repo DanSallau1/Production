d50a48eecb89b3093bfd00ffc4c8777822d5129b
CruzanCaramele <letters2hamza@gmail.com>
2016-02-24 17:54:27 +0800

    add script for consul installation

--- ops/packer/scripts/cloud.sh
+++ ops/packer/scripts/cloud.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -eux
+#!/bin/bash
 
 sudo wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
 sudo apt-get -y update
 sudo apt-get -y install puppet
-sudo apt-get -y install unzip
+sudo apt-get -y install unzip make git curl vim g++ libkrb5-dev
